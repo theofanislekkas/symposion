489789d0f90cf69754c851f5b910d54fdba0b86a
Theo Lekkas <theofanis.lekkas@theofanislekkas.com>
2015-01-28 10:38:32 -0500

    Uncomment admin display for proposals.

--- symposion/proposals/admin.py
+++ symposion/proposals/admin.py
@@ -4,28 +4,28 @@ from django.contrib import admin
 from symposion.proposals.models import ProposalSection, ProposalKind
 
 
-# admin.site.register(Proposal,
-#     list_display = [
-#         "id",
-#         "title",
-#         "speaker",
-#         "speaker_email",
-#         "kind",
-#         "audience_level",
-#         "cancelled",
-#     ],
-#     list_filter = [
-#         "kind__name",
-#         "result__accepted",
-#     ],
-#     actions = [export_as_csv_action("CSV Export", fields=[
-#         "id",
-#         "title",
-#         "speaker",
-#         "speaker_email",
-#         "kind",
-#     ])]
-# )
+admin.site.register(Proposal,
+    list_display = [
+        "id",
+        "title",
+        "speaker",
+        "speaker_email",
+        "kind",
+        "audience_level",
+        "cancelled",
+    ],
+    list_filter = [
+        "kind__name",
+        "result__accepted",
+    ],
+    actions = [export_as_csv_action("CSV Export", fields=[
+        "id",
+        "title",
+        "speaker",
+        "speaker_email",
+        "kind",
+    ])]
+)
 
 
 admin.site.register(ProposalSection)
